7b8d8f7c4ae8a9b248de67a24e2b58364744c758
Aditya Sodhani <adityassodhani92@gmail.com>
2017-06-06 18:42:52 -0500

    Fixed Confirmation error

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,7 @@ Rails.application.routes.draw do
   root 'web_pages#home'
 
 
-  # devise_scope :user do
-  # delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  # end
-
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
